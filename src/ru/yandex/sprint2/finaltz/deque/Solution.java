9e79fc06fd0029b89e5f3ef8b8a8f1502c713655
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-11-17 00:37:00 +0300

    Задача - Дек на кольцевом буфере

--- src/ru/yandex/sprint2/finaltz/deque/Solution.java
+++ src/ru/yandex/sprint2/finaltz/deque/Solution.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
+// Номер посылки: 97474348
+// Посылка: https://contest.yandex.ru/contest/22781/run-report/97474348/
 // A. Дек
 // https://contest.yandex.ru/contest/22781/problems/A/
 public class Solution {
@@ -84,91 +86,92 @@ public class Solution {
         System.out.println(stringBuilder);
     }
 
-    class Command {
-        String operand;
-        Integer param;
+}
 
-        public Command(String operand, Integer param) {
-            this.operand = operand;
-            this.param = param;
-        }
+class Command {
+    String operand;
+    Integer param;
 
-        @Override
-        public String toString() {
-            return "Command{" +
-                    "operand='" + operand + '\'' +
-                    ", param=" + param +
-                    '}';
+    public Command(String operand, Integer param) {
+        this.operand = operand;
+        this.param = param;
+    }
+
+    @Override
+    public String toString() {
+        return "Command{" +
+                "operand='" + operand + '\'' +
+                ", param=" + param +
+                '}';
+    }
+}
+
+class Deque {
+    private final Integer[] deque;
+    private final int capacity;
+    private int head;
+    private int tail;
+    private int size;
+
+    public Deque(int capacity) {
+        this.capacity = capacity;
+        this.head = 0;
+        this.tail = 1;
+        this.size = 0;
+        deque = new Integer[capacity];
+    }
+
+    public void push_front(int value) {
+        if (!isFull()) {
+            deque[head] = value;
+            if (--head < 0) {
+                head = capacity + head;
+            }
+            size++;
         }
     }
 
-    class Deque {
-        private final Integer[] deque;
-        private final int capacity;
-        private int head;
-        private int tail;
-        private int size;
-
-        public Deque(int capacity) {
-            this.capacity = capacity;
-            this.head = 0;
-            this.tail = 1;
-            this.size = 0;
-            deque = new Integer[capacity];
+    public void push_back(int value) {
+        if (!isFull()) {
+            deque[tail] = value;
+            tail = (tail + 1) % capacity;
+            size++;
         }
+    }
 
-        public void push_front(int value) {
-            if (!isFull()) {
-                deque[head] = value;
-                if (--head < 0) {
-                    head = capacity + head;
-                }
-                size++;
-            }
+    public Integer pop_front() {
+        if (isEmpty()) {
+            return null;
         }
+        head = (head + 1) % capacity;
+        Integer x = deque[head];
+        deque[head] = null;
+        size--;
+        return x;
+    }
 
-        public void push_back(int value) {
-            if (!isFull()) {
-                deque[tail] = value;
-                tail = (tail + 1) % capacity;
-                size++;
-            }
+    public Integer pop_back() {
+        if (isEmpty()) {
+            return null;
         }
+        if (--tail < 0) {
+            tail = tail + capacity;
+        }
+        Integer x = deque[tail];
+        deque[tail] = null;
+        size--;
+        return x;
+    }
 
-        public Integer pop_front() {
-            if (isEmpty()) {
-                return null;
-            }
-            head = (head + 1) % capacity;
-            Integer x = deque[head];
-            deque[head] = null;
-            size--;
-            return x;
-        }
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-        public Integer pop_back() {
-            if (isEmpty()) {
-                return null;
-            }
-            if (--tail < 0) {
-                tail = tail + capacity;
-            }
-            Integer x = deque[tail];
-            deque[tail] = null;
-            size--;
-            return x;
-        }
+    public boolean isFull() {
+        return size == capacity;
+    }
 
-        public boolean isEmpty() {
-            return size == 0;
-        }
-
-        private boolean isFull() {
-            return size == capacity;
-        }
-
-        public void print() {
-            System.out.println(Arrays.toString(deque));
-        }
+    public void print() {
+        System.out.println(Arrays.toString(deque));
     }
 }
