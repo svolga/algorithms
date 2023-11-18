af0e96448297f9c6c21d86d3f7227a7fc4a14bb4
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-11-18 21:58:55 +0300

    Задача - Дек на кольцевом буфере

--- src/ru/yandex/sprint2/finaltz/deque/Solution.java
+++ src/ru/yandex/sprint2/finaltz/deque/Solution.java
@@ -8,10 +8,43 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
-// Номер посылки: 97474348
-// Посылка: https://contest.yandex.ru/contest/22781/run-report/97474348/
+// Номер посылки: 97898875
+// https://contest.yandex.ru/contest/22781/run-report/97898875/
 // A. Дек
 // https://contest.yandex.ru/contest/22781/problems/A/
+
+/*
+-- ПРИНЦИП РАБОТЫ --
+Задача – реализовать Дек на кольцевом буфере.
+Дек – двусторонняя очередь, в которой элементы могут добавляться и удаляться как из начала, так и с конца очереди.
+Очередь – структура данных, поддерживающая принцип FIFO.
+Для хранения данных выбран массив с настраиваемыми параметрами:
+capacity – максимальная емкость массива и
+size – текущее количество элементов в массиве.
+Еще две переменные head и tail – хранят индексы текущего значения головы и хвоста Дека.
+
+При добавлении элемента в голову очереди происходит смещение индекса head - против часовой стрелки по кольцу,
+при добавлении в конец – по часовой стрелке. При удалении элементов из head и tail, смещение индексов происходит
+наоборот, для head – по часовой, для tail – против.
+
+Рассмотрим схематично массив в некоторый момент времени после наполнения тремя элементам.
+Например, массив:  [77, 33, null, null, null, 88] в котором добавлено 2 элемента в голову и 1 элемент в конец очереди
+можно представить на картинке:
+https://joxi.ru/KAgMR3KCEQ6Dem
+
+При добавлении еще одного элемента в голову Дека – будет добавлено значение в ячейку массива с индексом 4, а значение
+head сместится на одну позицию влево и станет равным 3.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+При работе с кольцевым буфером элементы добавляются и удаляются в массив фиксированной длины. Индексы элементов,
+над которыми совершаются операции - посчитаны заранее, поэтому временная сложность операций добавления и удаления
+выполняются за константное время O(1)
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --.
+Так как для хранения данных используется массив, то для добавления максимального количества, скажем n элементов,
+понадобится массив имеющий длину n, поэтому пространственная сложность составляет O(n).
+ */
+
 public class Solution {
 
     private final List<Command> commands;
@@ -56,28 +89,28 @@ public class Solution {
                     if (deque.isFull()) {
                         stringBuilder.append("error").append(System.lineSeparator());
                     } else {
-                        deque.push_front(command.param);
+                        deque.pushFront(command.param);
                     }
                     break;
                 case "pop_front":
                     if (deque.isEmpty()) {
                         stringBuilder.append("error").append(System.lineSeparator());
                     } else {
-                        stringBuilder.append(deque.pop_front()).append(System.lineSeparator());
+                        stringBuilder.append(deque.popFront()).append(System.lineSeparator());
                     }
                     break;
                 case "push_back":
                     if (deque.isFull()) {
                         stringBuilder.append("error").append(System.lineSeparator());
                     } else {
-                        deque.push_back(command.param);
+                        deque.pushBack(command.param);
                     }
                     break;
                 case "pop_back":
                     if (deque.isEmpty()) {
                         stringBuilder.append("error").append(System.lineSeparator());
                     } else {
-                        stringBuilder.append(deque.pop_back()).append(System.lineSeparator());
+                        stringBuilder.append(deque.popBack()).append(System.lineSeparator());
                     }
                     break;
             }
@@ -121,17 +154,15 @@ class Deque {
         deque = new Integer[capacity];
     }
 
-    public void push_front(int value) {
+    public void pushFront(int value) {
         if (!isFull()) {
             deque[head] = value;
-            if (--head < 0) {
-                head = capacity + head;
-            }
+            head = (head - 1 + capacity) % capacity;
             size++;
         }
     }
 
-    public void push_back(int value) {
+    public void pushBack(int value) {
         if (!isFull()) {
             deque[tail] = value;
             tail = (tail + 1) % capacity;
@@ -139,7 +170,7 @@ class Deque {
         }
     }
 
-    public Integer pop_front() {
+    public Integer popFront() {
         if (isEmpty()) {
             return null;
         }
@@ -150,13 +181,11 @@ class Deque {
         return x;
     }
 
-    public Integer pop_back() {
+    public Integer popBack() {
         if (isEmpty()) {
             return null;
         }
-        if (--tail < 0) {
-            tail = tail + capacity;
-        }
+        tail = (tail - 1 + capacity) % capacity;
         Integer x = deque[tail];
         deque[tail] = null;
         size--;
