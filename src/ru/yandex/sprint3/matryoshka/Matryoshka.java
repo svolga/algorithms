64e84b52df2507f314fef1f654858934bdcabcf0
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-11-18 09:00:44 +0300

    Рекурсия - матрешка

---
+++ src/ru/yandex/sprint3/matryoshka/Matryoshka.java
@@ -0,0 +1,23 @@
+package ru.yandex.sprint3.matryoshka;
+
+public class Matryoshka {
+    public static void buildMatryoshka(int size, int n) {
+        if (n >= 1) {
+
+            System.out.println("above n = " + n + "; size = " + size);
+
+            System.out.println("Создаём низ матрёшки размера " + size + ".");
+            buildMatryoshka(size - 1, n - 1);
+
+            System.out.println("below n = " + n + "; size = " + size);
+
+            System.out.println("Создаём верх матрешки размера " + size + ".");
+        } else {
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        buildMatryoshka(4, 3);
+    }
+}
