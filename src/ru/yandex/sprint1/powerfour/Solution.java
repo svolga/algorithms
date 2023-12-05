a9884a0855296cfb2305e2e8b6f034c6f05d90f8
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-12-05 20:24:34 +0300

    Задача - Степень четырёх

---
+++ src/ru/yandex/sprint1/powerfour/Solution.java
@@ -0,0 +1,36 @@
+package ru.yandex.sprint1.powerfour;
+
+// I. Степень четырёх
+// https://contest.yandex.ru/contest/22449/problems/I/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        new Solution().run(args);
+    }
+
+    private void run(String[] args) throws IOException {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int value = Integer.parseInt(reader.readLine());
+            boolean isValid = isPower(value);
+            System.out.println(isValid ? "True" : "False");
+        }
+    }
+
+    private boolean isPower(int value) {
+
+        int result = 1;
+        int j = 0;
+        while (result < value) {
+            result *= 4;
+            j++;
+        }
+        return result == value;
+    }
+
+}
