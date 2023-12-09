496ddf30de12ec26abd2cc0a03a8ecd6637fd5d4
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-12-09 23:57:35 +0300

    Задача - Полиномиальный хеш

---
+++ src/ru/yandex/sprint4/polinomhash/Solution.java
@@ -0,0 +1,38 @@
+package ru.yandex.sprint4.polinomhash;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// A. Полиномиальный хеш
+// https://contest.yandex.ru/contest/23991/problems/
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        new Solution().run(args);
+    }
+
+    private void run(String[] args) throws IOException {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+ /*           int a = 123; // основание
+            String str = "HaSH"; // hash -> 6080 ; HaSH -> 56156
+            int m = 100003;
+*/
+            int a = Integer.parseInt(reader.readLine());
+            int m = Integer.parseInt(reader.readLine());
+            String str = reader.readLine();
+
+            long h = hash(str, a, m);
+            System.out.println(h);
+        }
+    }
+
+    private long hash(String str, int a, int m) {
+        long h = 0;
+        for (int i = 0; i < str.length(); i++) {
+            h = (h * a + (int) str.charAt(i)) % m;
+        }
+        return h;
+    }
+}
