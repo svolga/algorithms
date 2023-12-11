875f6bf17d5dd11cc4136bf5a25e9dcd34278334
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-12-12 00:23:47 +0300

    Задача - Сломай меня

---
+++ src/ru/yandex/sprint4/breakhash/Solution.java
@@ -0,0 +1,54 @@
+package ru.yandex.sprint4.breakhash;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+// B. Сломай меня
+// https://contest.yandex.ru/contest/23991/problems/B/
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().run(args);
+    }
+
+    private String getRandomString(int length) {
+        String chars = "abcdefjhijklmnopqrstuvwxyz";
+
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(ThreadLocalRandom.current().nextInt(0, chars.length())));
+        }
+        return sb.toString();
+    }
+
+    private void run(String[] args) {
+
+        int a = 1000;
+        int m = 123_987_123;
+
+        int len1 = 20;
+// khjnxoojcx --> yucmjbdzjk
+// zhijviuehkuudsshxxjw --> iycpazcylxdfjploynzc
+        int count = 100_000;
+        Map<Long, String> map = new HashMap<>();
+        for (int i = 0; i < count; i++) {
+            String str = getRandomString(len1);
+            long h = hash(str, a, m);
+            if (map.containsKey(h) && !str.equals(map.get(h))) {
+                System.out.println("FOUND: hash " + h + " Строки: " + map.get(h) + " --> " + str);
+//                break;
+            } else {
+                map.put(h, str);
+            }
+        }
+    }
+
+    private long hash(String str, int a, int m) {
+        long h = 0;
+        for (int i = 0; i < str.length(); i++) {
+            h = (h * a + (int) str.charAt(i)) % m;
+        }
+        return h;
+    }
+}
---
+++ src/ru/yandex/sprint4/bucket/Solution.java
@@ -0,0 +1,55 @@
+package ru.yandex.sprint4.bucket;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().run(args);
+    }
+
+    private void print(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    private void run(String[] args) {
+
+
+        int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        print(nums);
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        print(nums);
+
+        String abc = "abc";
+
+        for (char c : abc.toCharArray()) {
+
+        }
+
+        for (char c : abc.toCharArray()) {
+            System.out.println(c + " --> " + (int) c);
+        }
+
+        /*
+        int k = "temp".hashCode();
+        int t = (String.valueOf(k)).hashCode();
+*/
+    }
+
+
+    private double rest(int x, int y) {
+        return Math.floorMod(x, y);
+    }
+
+}
