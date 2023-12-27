ea71f9e03c87f851a50d6cdc88e550632f7a6a09
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-12-27 13:52:03 +0300

    Задача - Метод скользящего окна

--- src/leetcode/slidewindow2/Solution.java
+++ src/leetcode/slidewindow2/Solution.java
@@ -1,13 +1,15 @@
 package leetcode.slidewindow2;
 
-// Мнтод скользящего окна
+import java.util.HashSet;
+import java.util.Set;
+
+// Метод скользящего окна
 // Временная сложность O(n), пространственная сложность O(n)
 // 3. Longest Substring Without Repeating Characters
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 public class Solution {
 /*
 Подход двух указателей
-
 int r = 0;
 for (int l=0; l<n; l++){
     while(r<n && [отрезок хороший]){
@@ -18,59 +20,20 @@ for (int l=0; l<n; l++){
 }
 */
 
-    boolean contains(StringBuilder sb, String findString) {
-        return sb.indexOf(findString) > -1;
-    }
-/*
     public int lengthOfLongestSubstring(String s) {
-
-        String current = "";
-        String maxString = "";
+        int n = s.length();
+        int l = 0;
         int r = 0;
-        for (int l = 0; l < s.length(); l++) {
-
-            boolean isFound = false;
-
-            while (r < s.length() && !current.contains(Character.toString(s.charAt(r)))) {
-                current += s.charAt(r);
-                isFound = true;
-                r += 1;
-            }
-
-            if (!isFound){
-                current = s.substring(l, r);
-            }
-
-            if (maxString.length() <= current.length()) {
-                maxString = current;
-            }
-
-        }
-
-        System.out.println(maxString);
-
-        return maxString.length();
-    }
-*/
-
-
-    public int lengthOfLongestSubstring(String s) {
-        String current = "";
         int max = 0;
-        int r = 0;
-        for (int l = 0; l < s.length(); l++) {
-
-            while (r < s.length() && !current.contains(Character.toString(s.charAt(r)))) {
-                current += s.charAt(r);
-                r += 1;
-            }
-
-            System.out.println("l = " + l + "; r = " + r);
-
-            current = s.substring(l, r);
-
-            if (max <= current.length()) {
-                max = current.length();
+        Set<Character> set = new HashSet<>();
+        while (r < n && l < n) {
+            if (!set.contains(s.charAt(r))) {
+                set.add(s.charAt(r));
+                r++;
+                max = Math.max(max, r - l);
+            } else {
+                set.remove(s.charAt(l));
+                l++;
             }
         }
         return max;
@@ -79,7 +42,8 @@ for (int l=0; l<n; l++){
 
     private void run(String[] args) {
 //        String row = "bbbbb";
-        String row = "pwwkew";
+//        String row = "pwwkew";
+        String row = "abcabcbb";
         int length = lengthOfLongestSubstring(row);
         System.out.println("length = " + length);
     }
