8f16d1ad548231dc24f6da13fc55634b4df76646
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2023-12-30 19:50:42 +0300

    Задача - A. Лампочки

---
+++ src/ru/yandex/sprint5/tree/Solution.java
@@ -0,0 +1,40 @@
+package ru.yandex.sprint5.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+// Дерево с произвольным кол-ом детей
+class Node<T> {
+    private T obj;
+    private List<Node<T>> children;
+
+    public Node(T obj) {
+        this.obj = obj;
+        this.children = new ArrayList<>();
+    }
+
+    public Node(T obj, List<Node<T>> children) {
+        this.obj = obj;
+        this.children = children != null ? children : new ArrayList<>();
+    }
+}
+
+// Бинарное дерево
+class NodeBin {
+    public NodeBin(int obj, Node left, Node right) {
+        this.obj = obj;
+        this.left = left;
+        this.right = right;
+    }
+    public int obj;
+    public Node left;
+    public Node right;
+}
