f2b03eee013c245e6f3d0d5933d03000f8d36161
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-01-03 22:43:50 +0300

    Задача - 938. Range Sum of BST

---
+++ src/leetcode/binarytree/rangesumofbst/Solution.java
@@ -0,0 +1,49 @@
+package leetcode.binarytree.rangesumofbst;
+
+// 938. Range Sum of BST
+// https://leetcode.com/problems/range-sum-of-bst/description/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().run(args);
+    }
+
+    private void run(String[] args) {
+
+    }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return 0;
+
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        } else if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+    }
+
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
