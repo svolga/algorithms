90721246e8fc768f3d16699f24a680ecf8f17a4d
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-01-23 14:50:06 +0300

    Задача - Два указателя

---
+++ src/other/slidingwindow/twosum/Solution.java
@@ -0,0 +1,44 @@
+package other.slidingwindow.twosum;
+
+import java.util.Arrays;
+
+// Два указателя
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().run(args);
+    }
+
+    private void run(String[] args) {
+        int[] array = {1, 2, 3, 3, 5, 6, 7, 8, 9, 9};
+        int target = 111;
+        boolean res = twoSumSlow(array, target);
+        System.out.println("array = " + Arrays.toString(array) +
+                "; target = " + target + "; res = " + res);
+
+    }
+
+    private boolean twoSumSlow(int[] input, int targetValue) {
+
+        int left = 0;
+        int right = input.length - 1;
+
+        while (left < right) {
+
+            if (input[left] + input[right] == targetValue) {
+                return true;
+            }
+
+            if (input[left] + input[right] < targetValue){
+                left++;
+            }
+
+            if (input[left] + input[right] > targetValue){
+                right--;
+            }
+        }
+
+        return false;
+    }
+
+}
