f4ce01d3b651b80b302f02b7ee391ee9d6d86137
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-02-06 20:06:07 +0300

    Задача - D. BFS

---
+++ src/ru/yandex/sprint6/dfs/Solution.java
@@ -0,0 +1,18 @@
+package ru.yandex.sprint6.dfs;
+
+// C. DFS
+// https://contest.yandex.ru/contest/25069/problems/C/
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().run(args);
+
+    }
+
+    private void run(String[] args) {
+
+
+    }
+
+
+}
