3e445b7bccdd071988b011cf42ecc05357d8c8a9
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2026-03-10 17:03:38 +0300

    feat: Найти сумму самых  больших значений в массиве

---
+++ src/main/java/stepik/binary/brootforce/Main.java
@@ -0,0 +1,39 @@
+package stepik.binary.brootforce;
+
+import org.junit.jupiter.api.Test;
+/*
+    Найти сумму двух максимальных чисел за O(N)
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Main {
+
+    private int getTwoMax(int[] array) {
+
+        int max1 = 0;
+        int max2 = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > max1) {
+                max2 = max1;
+                max1 = array[i];
+            } else if (array[i] > max2) {
+                max2 = array[i];
+            }
+        }
+        return max1 + max2;
+    }
+
+    private void testing(int actual, int expected) {
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void test() {
+
+        testing(getTwoMax(new int[]{20, 10, 1, 5, 6, 20, 100, 99, 3}), 199);
+
+    }
+
+}
