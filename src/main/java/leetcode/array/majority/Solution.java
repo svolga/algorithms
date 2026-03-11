74d1d78fcca27a0c6018d07787441278d6d0cca1
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2026-03-11 09:43:40 +0300

    feat: 169. Majority Element

---
+++ src/main/java/leetcode/array/majority/Solution.java
@@ -0,0 +1,45 @@
+package leetcode.array.majority;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+169. Majority Element
+https://leetcode.com/problems/majority-element/description/?envType=problem-list-v2&envId=hash-table
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+ */
+public class Solution {
+
+    public int majorityElement(int[] nums) {
+        int count = nums.length / 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) > count) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+
+
+    private void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test() {
+        testing(majorityElement(new int[]{3, 2, 3}), 3);
+    }
+
+}
