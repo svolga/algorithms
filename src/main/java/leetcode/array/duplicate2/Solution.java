4c1b2b959713b219c3ef862a53e5559560f5612b
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2026-03-11 22:54:44 +0300

    feat: 219. Contains Duplicate II

--- src/main/java/leetcode/array/duplicate2/Solution.java
+++ src/main/java/leetcode/array/duplicate2/Solution.java
@@ -3,12 +3,9 @@ package leetcode.array.duplicate2;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
