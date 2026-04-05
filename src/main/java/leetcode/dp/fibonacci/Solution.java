e3b457351fd3720ab6e1cb0d7da77cae46b37c35
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2026-04-05 21:12:27 +0300

    feat: 509. Fibonacci Number

--- src/main/java/leetcode/dp/fibonacci/Solution.java
+++ src/main/java/leetcode/dp/fibonacci/Solution.java
@@ -1,50 +1,40 @@
-package java.leetcode.dp.fibonacci;
+package leetcode.dp.fibonacci;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // 509. Fibonacci Number
 // https://leetcode.com/problems/fibonacci-number/description/
 public class Solution {
 
-    public static void main(String[] args) {
-        new Solution().run(args);
-    }
-
-    private void run(String[] args) {
-        int n = 7;
-        int res = fib(n);
-        System.out.println(res);
-    }
-
     public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
-        if (n == 0 || n == 1) {
-            return n;
-        }
-
-        int n1 = 0;
-        int n2 = 1;
-
+        int num0 = 0;
+        int num1 = 1;
         int sum = 0;
         for (int i = 2; i <= n; i++) {
-            sum = n1 + n2;
-            n1 = n2;
-            n2 = sum;
+            sum = num0 + num1;
+            num0 = num1;
+            num1 = sum;
         }
-
         return sum;
     }
 
 
-/*
-    public int fib(int n) {
-        List<Integer> list = new ArrayList<>();
-        list.add(0, 0);
-        list.add(1, 1);
-
-        for (int i = 2; i <= n; i++) {
-            list.add(i, list.get(i - 1) + list.get(i - 2));
-        }
-
-        return list.get(n);
+    private void testing(int actual, int expected) {
+        assertEquals(expected, actual);
     }
-*/
+
+    @Test
+    public void test() {
+        testing(fib(2), 1);
+        testing(fib(3), 2);
+        testing(fib(4), 3);
+        testing(fib(5), 5);
+        testing(fib(6), 8);
+    }
+
 }
