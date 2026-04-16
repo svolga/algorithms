a294177718d13ac1efa2f510eb382a167feabfe2
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2026-04-16 15:48:46 +0300

    feat: бинарный поиск

--- src/main/java/stepik/binary/findstream/Solution.java
+++ src/main/java/stepik/binary/findstream/Solution.java
@@ -1,8 +1,6 @@
 package stepik.binary.findstream;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Vector;
 import java.util.stream.IntStream;
