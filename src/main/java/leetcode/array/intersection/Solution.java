2ecfd736636a11eeb597f2d2295381c76fc3cb9d
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2026-05-05 12:36:23 +0300

    feat: 350. Intersection of Two Arrays II

---
+++ src/main/java/leetcode/array/intersection/Solution.java
@@ -0,0 +1,56 @@
+package leetcode.array.intersection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+/*
+350. Intersection of Two Arrays II
+https://leetcode.com/problems/intersection-of-two-arrays-ii/
+ */
+public class Solution {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i = 0;
+        int j = 0;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (i <= nums1.length - 1 && j <= nums2.length - 1) {
+
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            }
+
+        }
+
+        return list.stream().mapToInt(x -> x).toArray();
+    }
+
+    private void testing(int[] actual, int[] expected) {
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test() {
+        testing(intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}), new int[]{2, 2});
+        testing(intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}), new int[]{4, 9});
+    }
+
+
+}
